5cbff34d051c403024eb3f2d12294165be374cd1
Vincenzo-23 <specialevincenzo88@gmail.com>
2024-03-01 12:44:25 +0100

    ADD: added js file with the exercises

---
+++ js/script.js
@@ -0,0 +1,26 @@
+//Esercizio 1
+let number = 100; //number
+
+console.log(number);
+
+// Esercizio 2
+const PI = 3.14; //number
+
+console.log(PI);
+
+//Esercizio 2/B
+//const PI = 3.146; //number
+//impossibile cambiare valore ad una const
+
+//Esercizio 2/C
+let radius = 8; //number
+let circle = radius*2*PI; //number
+
+console.log("Circle area",circle);
+
+//Esercizio 3
+let name = "Carlo"; //string
+name = "Marco"; //string
+
+console.log(name);
+
